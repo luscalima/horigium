424708d6c162e2f716fe9e9cf45e84c889360b76
Lusca Lima <lusca.lima@outlook.com>
2023-01-07 13:28:53 -0300

    feat: create app button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="relative flex justify-center items-center h-12 pl-6 pr-16 rounded-full uppercase text-stone-900 bg-stone-100"
+  >
+    <span class="tracking-widest font-medium">
+      <slot />
+    </span>
+    <span
+      class="absolute right-1 flex justify-center items-center w-10 h-10 text-2xl rounded-full text-stone-100 bg-stone-900"
+    >
+      <slot name="icon" />
+    </span>
+  </button>
+</template>
