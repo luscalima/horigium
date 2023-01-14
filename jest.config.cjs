7d9562d9fe4c06a280db7e5e79afb46e644c9a20
LuscaLima <lusca.lima@outlook.com>
2023-01-14 12:57:54 -0300

    chore: add .ts file in trasform rules

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,12 +2,13 @@ module.exports = {
   preset: "ts-jest",
   globals: {},
   testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.vue$": "@vue/vue3-jest",
-    "^.+\\js$": "babel-jest",
-  },
-  moduleFileExtensions: ["vue", "js", "json", "jsx", "ts", "tsx", "node"],
   testEnvironmentOptions: {
     customExportConditions: ["node", "node-addons"],
   },
+  transform: {
+    "^.+\\.vue$": "@vue/vue3-jest",
+    "^.+\\js$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["vue", "js", "json", "jsx", "ts", "tsx", "node"],
 };
